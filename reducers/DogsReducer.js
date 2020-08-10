4c4c0f9fcb9d33ed729e50e03f50e0c8d4ee7333
eshan1799 <eshan1799@gmail.com>
2020-08-10 23:38:46 +0100

    add dog reducer

---
+++ reducers/DogsReducer.js
@@ -0,0 +1,26 @@
+const initState = { dogs: ['chow chow', 'golden retriever', 'samoyed', 'german shepard'], favourite: null }
+
+const dogsReducer = (state=initState, action) => {
+
+    switch(action.type) {
+        case 'ADD_A_DOG':
+            const newDog = action.payload;
+        return { ...state, dogs: [...state.dogs, newDog] };
+        
+        case 'SELECT_FAVE_DOG':
+            const favDog = state.dogs.find( d => d.id === action.payload.id);
+        return { ...state, favourite: favDog }
+
+        case 'UPDATE_A_DOG':
+            const dogToUpdate = state.dogs.find( d => d.id === action.payload.id);
+            const index = state.dogs.indexOf(dogToUpdate);
+            const updatedDogs = [ ...state.dogs.slice(0, index), action.payload, ...state.dogs.slice(index + 1, state.dogs.length - 1) ]
+        return { ...state, dogs: updatedDogs }
+
+        default:
+            return state 
+    }
+
+}
+
+export default dogsReducer
